254b7d9969af5c9965019c5d1b100a6ad1a2a2f6
Mac <mac@Macs-iMac.hitronhub.home>
2020-07-07 16:44:38 -0700

    getting ready to create apollo server

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,11 +1,7 @@
 import React from 'react'
-import { Hello } from './Hello.js'
-import { Info } from './Info.js'
 
 export const App = () => (
 	<div>
 		<h1>Welcome to Meteor!</h1>
-		<Hello />
-		<Info />
 	</div>
 )
