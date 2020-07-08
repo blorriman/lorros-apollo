40abab5c3918624038ed0b3c10858bb3a75ab946
Bob Lorriman <blorriman@Bobs-MBP.hitronhub.home>
2020-07-08 14:02:26 -0700

    added Apollo server and client

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,1 +1,27 @@
-console.log('hi from server')
+import { ApolloServer, gql } from 'apollo-server-express'
+import { WebApp } from 'meteor/webapp'
+import { getUser } from 'meteor/apollo'
+
+const typeDefs = `
+type Query {
+  hi: String
+}
+`
+
+const resolvers = {
+	Query: {
+		hi() {
+			return 'Hello World!'
+		},
+	},
+}
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+})
+
+server.applyMiddleware({
+	app: WebApp.connectHandlers,
+	path: '/graphql',
+})
